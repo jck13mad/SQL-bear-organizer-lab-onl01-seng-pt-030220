204f6e55370c0af4629c40f76222a796dc218488
Jack Madden <jck13mad@gmail.com>
2020-04-30 18:34:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "brown", "mean", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "nice", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "white", "ok", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "brown",  "whatever", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "brown",  "goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "black", "mean", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "M", "brown", "ok", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "white", "whatever", 0);
